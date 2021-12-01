a0d1e0034088630de4bf3330f0612f4004b01801
sebdotv <sebdotv@gmail.com>
2021-12-01 13:23:27 +0100

    rewrite countInc using standard primitives

--- src/main/scala/d01.scala
+++ src/main/scala/d01.scala
@@ -1,18 +1,6 @@
-import scala.annotation.tailrec
-
 object d01 {
-  def countInc(list: List[Int]): Int = {
-    @tailrec
-    def it(l: List[Int], acc: Int, prev: Option[Int]): Int = {
-      l match {
-        case Nil => acc
-        case h :: t =>
-          val i = prev.map(p => if (p < h) 1 else 0).getOrElse(0)
-          it(t, acc + i, Some(h))
-      }
-    }
-    it(list, 0, None)
-  }
+  def countInc(list: List[Int]): Int =
+    list.drop(1).zip(list).count { case (a, b) => a > b }
 
   def countIncSliding(list: List[Int]): Int =
     countInc(slidingSum(list))
