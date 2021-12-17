c8e09707a3e89bb6732e58cd9bd9edeb19601d53
sebdotv <sebdotv@gmail.com>
2021-12-17 21:57:08 +0100

    rename stuff

--- src/main/scala/aoc/Graph.scala
+++ src/main/scala/aoc/Graph.scala
@@ -5,12 +5,15 @@ import cats.data.NonEmptyList
 object Graph {
   type Path[V] = NonEmptyList[V]
   type Paths[V] = List[Path[V]]
+  type NeighborsFunction[V] = V => List[V]
+  type VisitedFunction[V] = (Path[V], V) => Boolean
 
-  def dfs[V](edgesF: V => List[V])(visitedF: (Path[V], V) => Boolean)(start: V): Paths[V] = {
-    def it(path: Path[V], results: Paths[V]): Paths[V] =
-      edgesF(path.head) match {
-        case Nil   => path :: results
-        case edges => edges.filterNot(visitedF(path, _)).foldLeft(results)((b, next) => it(next :: path, b))
+  // todo make tailrec
+  def dfs[V](neighborsF: NeighborsFunction[V])(visitedF: VisitedFunction[V])(start: V): Paths[V] = {
+    def it(path: Path[V], acc: Paths[V]): Paths[V] =
+      neighborsF(path.head) match {
+        case Nil       => path :: acc
+        case neighbors => neighbors.filterNot(visitedF(path, _)).foldLeft(acc)((b, next) => it(next :: path, b))
       }
     it(NonEmptyList.one(start), Nil).map(_.reverse)
   }
