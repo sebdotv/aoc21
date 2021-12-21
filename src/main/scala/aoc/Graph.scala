66e8bb54d4012eda794f418aee0205ec0ba875dc
sebdotv <sebdotv@gmail.com>
2021-12-21 11:30:59 +0100

    add jgrapht A*

--- src/main/scala/aoc/Graph.scala
+++ src/main/scala/aoc/Graph.scala
@@ -3,14 +3,18 @@ package aoc
 import cats.Eq
 import cats.data.NonEmptyList
 import cats.implicits._
+import org.jgrapht.alg.interfaces.AStarAdmissibleHeuristic
+import org.jgrapht.alg.shortestpath.AStarShortestPath
+import org.jgrapht.graph.{DefaultWeightedEdge, SimpleDirectedWeightedGraph}
+
+import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 object Graph {
   type Path[V] = NonEmptyList[V]
   type Paths[V] = List[Path[V]]
   type NeighborsFunction[V] = V => List[V]
-  type LengthFunction[V, W] = (V, V) => W
+  type WeightFunction[V, W] = (V, V) => W
   type VisitedFunction[V] = (Path[V], V) => Boolean
-  type HeuristicFunction[V, W] = V => W
   object VisitedFunction {
     def fromEq[V: Eq]: VisitedFunction[V] = _.contains_(_)
   }
@@ -26,15 +30,15 @@ object Graph {
   }
 
   // https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode
-  def aStar[V: Eq](
+  def aStarSlow[V: Eq](
       neighborsF: NeighborsFunction[V]
-  )(weightF: LengthFunction[V, Double])(heuristicF: HeuristicFunction[V, Double])(start: V, goal: V): Path[V] = {
+  )(weightF: WeightFunction[V, Double])(heuristicF: WeightFunction[V, Double])(start: V, goal: V): Path[V] = {
     import collection.mutable
 
     // The set of discovered nodes that may need to be (re-)expanded.
     // Initially, only the start node is known.
     // This is usually implemented as a min-heap or priority queue rather than a hash-set.
-    // todo switch impl
+    // todo slow, must switch impl (PriorityQueue was tried, but remove/update is too slow)
     val openSet = mutable.Set(start)
 
     // For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start
@@ -48,7 +52,7 @@ object Graph {
     // For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
     // how short a path from start to finish can be if it goes through n.
     val fScore = mutable.Map.empty[V, Double].withDefaultValue(Double.PositiveInfinity)
-    fScore(start) = heuristicF(start)
+    fScore(start) = heuristicF(start, goal)
 
     while (openSet.nonEmpty) {
       // This operation can occur in O(1) time if openSet is a min-heap or a priority queue
@@ -71,7 +75,7 @@ object Graph {
             // This path to neighbor is better than any previous one. Record it!
             cameFrom(neighbor) = current
             gScore(neighbor) = tentative_gScore
-            fScore(neighbor) = tentative_gScore + heuristicF(neighbor)
+            fScore(neighbor) = tentative_gScore + heuristicF(neighbor, goal)
             if (!openSet.contains(neighbor)) {
               openSet.add(neighbor)
             }
@@ -83,7 +87,29 @@ object Graph {
     throw new RuntimeException("Failure")
   }
 
-  def dijkstra[V](neighborsF: NeighborsFunction[V])(lengthF: LengthFunction[V, Int])(vertices: List[V], source: V) = {
+  def aStar[V: Eq](
+      neighborsF: NeighborsFunction[V]
+  )(weightF: WeightFunction[V, Double])(heuristicF: WeightFunction[V, Double])(vertices: List[V])(start: V, goal: V): Path[V] = {
+    val graph = new SimpleDirectedWeightedGraph[V, DefaultWeightedEdge](classOf[DefaultWeightedEdge])
+    for (v <- vertices) {
+      graph.addVertex(v)
+    }
+    for (v <- vertices) {
+      for (n <- neighborsF(v)) {
+        graph.addEdge(v, n)
+        graph.setEdgeWeight(v, n, weightF(v, n))
+      }
+    }
+    val admissibleHeuristic = new AStarAdmissibleHeuristic[V] {
+      override def getCostEstimate(sourceVertex: V, targetVertex: V): Double =
+        heuristicF(sourceVertex, targetVertex)
+    }
+    val algo = new AStarShortestPath(graph, admissibleHeuristic)
+    val path = algo.getPath(start, goal)
+    NonEmptyList.fromListUnsafe(path.getVertexList.asScala.toList)
+  }
+
+  def dijkstra[V](neighborsF: NeighborsFunction[V])(lengthF: WeightFunction[V, Int])(vertices: List[V], source: V) = {
     import collection.mutable
 
     val dist = mutable.Map.empty[V, Int]
